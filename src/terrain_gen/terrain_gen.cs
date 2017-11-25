938d91a1f609fddf9e8f5f48dc4df56d296aea85
RomanSC <r.s.col988@gmail.com>
2017-11-25 14:12:50 -0500

    Remove unecessary tests

--- src/terrain_gen/terrain_gen.cs
+++ src/terrain_gen/terrain_gen.cs
@@ -10,6 +10,27 @@ namespace Main {
     public partial class TerrainGen {
         public double seed;
 
-        Graph g = new Graph();
+        public int chunk_size;
+        public int view_distance; // In number of chunks away from the player
+        public int world_size;
+        public Graph world;
+
+        /* Graph g = new Graph(world_size); */
+
+        public TerrainGen(int chunk_size, int view_distance, int world_size) {
+            this.chunk_size = chunk_size;
+            this.view_distance = view_distance;
+            this.world_size = world_size;
+
+            this.world = new Graph(world_size);
+        }
+
+    }
+
+    class Tests {
+        static void Main(string[] args) {
+            TerrainGen terrain = new TerrainGen(10, 10, 500);
+            Console.WriteLine(terrain.world.getString());
+        }
     }
 }
