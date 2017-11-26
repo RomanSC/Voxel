0c4126f3a74bf556adbf9ceeece33ba31f6b3751
Roman S. Collins <r.s.col988@gmail.com>
2017-11-26 13:09:51 -0500

    Better graph.cs

--- src/graph/graph.cs
+++ src/graph/graph.cs
@@ -1,32 +1,28 @@
 /* graph.cs | Roman S. Collins | Thu, Nov 2016
-
-
-
 */
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
-namespace Main {
+namespace Main
+{
     public partial class Graph {
-        public IDictionary<Tuple<int, int>, List<int>> grid = new Dictionary<Tuple<int, int>, List<int>>();
+        public IDictionary<(int, int), List<int>> grid = new Dictionary<(int, int), List<int>>();
         public int size;
         public int width;
         public int height;
 
         // Constructor
         public Graph(int size) {
-            this.grid = new Dictionary<Tuple<int, int>, List<int>>();
+            this.grid = new Dictionary<(int, int), List<int>>();
             this.size = size; this.width = this.size; this.height = this.size;
 
             for (int y = 0; y < this.height; y++) {
                 for (int x = 0; x < this.width; x++) {
                     /* Console.WriteLine(string.Format("{0}, {1}", y, x)); */
                     /* grid.Add(y, "test"); */
-                    Tuple<int, int> tup = new Tuple<int, int>(y, x);
+                    var tup = (y, x);
                     List<int> nl = new List<int> {0, 0, 0, 0};
 
                     /* Console.WriteLine(string.Format("{0}", tup)); */
@@ -41,40 +37,38 @@ namespace Main {
         }
 
         public string getString() {
-            string outstr = "{";
+            StringBuilder outstr = new StringBuilder(capacity: this.width * this.height * 15);
 
             for (int y = 0; y < this.width; y++) {
                 for (int x = 0; x < this.height; x++) {
-                    string outoutstr = "[";
+                    outstr.Append('(').Append(y).Append(", ").Append(x).Append("): ").Append('[');
 
                     // Lim to size of list contained in dictionary as values
-                    int lim = grid[new Tuple<int, int>(y, x)].Count;
+                    int lim = grid[(y, x)].Count;
 
                     for (int i = 0; i < lim; i++) {
                         if (i < lim - 1) {
-                            outoutstr = outoutstr + string.Format("{0}, ", grid[new Tuple<int, int>(y, x)][i]);
+                            outstr.Append(grid[(y, x)][i]).Append(", ");
                         } else {
-                            outoutstr = outoutstr + string.Format("{0}", grid[new Tuple<int, int>(y, x)][i]);
+                            outstr.Append(grid[(y, x)][i]);
                         }
                     }
 
-                    outoutstr = outoutstr + "]";
-
-                    outstr = outstr + string.Format("({0}, {1}): {2}", y, x, outoutstr);
+                    outstr.Append(']');
 
                     if (x < (this.width - 1)) {
-                        outstr = outstr + " ";
+                        outstr.Append(' ');
                     }
                 }
 
                 if (y < (this.height - 1)) {
-                    outstr = outstr + "\n";
+                    outstr.Append('\n');
                 }
             }
 
-            outstr = outstr + "}";
+            outstr.Append('}');
 
-            return outstr;
+            return outstr.ToString();
         }
 
         // Get value for z
@@ -85,7 +79,7 @@ namespace Main {
                     val = null;
                 }
 
-                Tuple<int, int> new_tup = new Tuple<int, int>(y, x);
+                var new_tup = (y, x);
                 val = this.grid[new_tup][0];
 
                 return val;
@@ -99,20 +93,20 @@ namespace Main {
                 }
 
                 /* Tuple new_tup = new Tuple<int, int>(y, x); */
-                Tuple<int, int> new_tup = new Tuple<int, int>(y, x);
+                var new_tup = (y, x);
                 this.grid[new_tup][0] = val;
         }
 
         // Get color for coord
         public List<int> getColor(int y, int x) {
-            Tuple<int, int> new_tup = new Tuple<int, int>(y, x);
+            var new_tup = (y, x);
             List<int> outlist = new List<int> {this.grid[new_tup][1], this.grid[new_tup][2], this.grid[new_tup][3]};
             return outlist;
         }
 
         // Set color for coord
         public void setColor(int y, int x, int r, int g, int b) {
-            Tuple<int, int> new_tup = new Tuple<int, int>(y, x);
+            var new_tup = (y, x);
             this.grid[new_tup][1] = r; this.grid[new_tup][2] = g; this.grid[new_tup][3] = b;
         }
     }
@@ -122,4 +116,3 @@ namespace Main {
     /*     } */
     /* } */
 }
-
